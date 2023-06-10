e1eef5c4ceba2844d8340bd074b3de63f5f33999
Chaitanya Tripathi <93254346+chaitanyatripathi0618@users.noreply.github.com>
2023-06-10 15:04:42 +0530

    Create SearchRange.java

---
+++ SearchRange.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first=-1, second=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                first=i;
+                break;
+
+            }
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]==target){
+                second=i;
+                break;
+            }
+        }
+        return new int[]{first, second};
+    }
+}
