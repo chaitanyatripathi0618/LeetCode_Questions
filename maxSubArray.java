ef03f848e27c6a264872758a670b535c7561bf29
Chaitanya Tripathi <93254346+chaitanyatripathi0618@users.noreply.github.com>
2023-06-11 03:23:50 +0530

    Create maxSubArray.java

---
+++ maxSubArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxc=-99999999;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>maxc){
+                maxc=sum;
+            
+            }
+            if(sum<0){
+                sum=0;
+
+            }
+
+            
+        }
+        return maxc;
+
+    }
+}
